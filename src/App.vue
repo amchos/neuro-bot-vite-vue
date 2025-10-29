2817a05a741d2f86291ec17a3ebc1f7a975a2dd0
amchos <124582071+amchos@users.noreply.github.com>
2025-10-29 13:49:53 +0400

    initData test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const tg = window.Telegram.WebApp
+  tg.ready() // сигнал Telegram, что WebApp готов
+  alert('initData:', tg.initData)
+  alert('initDataUnsafe:', tg.initDataUnsafe)
+})
 </script>
 
 <template>
-<h1>Hello, this is test</h1>
+  <div>
+    <h1>Telegram WebApp Init Test</h1>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
