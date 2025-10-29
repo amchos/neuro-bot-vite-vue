f4bb2874400f835af9fe849d9d604bb9199e0423
amchos <124582071+amchos@users.noreply.github.com>
2025-10-29 13:57:15 +0400

    init data test 2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
-import { onMounted } from 'vue'
+import WebApp from '@twa-dev/sdk'
+import { ref, onMounted } from 'vue'
 
+const user = ref(null)
 onMounted(() => {
-  const tg = window.Telegram.WebApp
-  tg.ready() // сигнал Telegram, что WebApp готов
-  alert('initData:', tg.initData)
-  alert('initDataUnsafe:', tg.initDataUnsafe)
+  console.log('initData:', WebApp.initData)
+  console.log('initDataUnsafe:', WebApp.initDataUnsafe)
+  user.value = WebApp.initDataUnsafe.user
 })
 </script>
 
 <template>
   <div>
-    <h1>Telegram WebApp Init Test</h1>
+    <h1>Telegram WebApp InitData Test</h1>
+    <h2>{{ JSON.parse(user) }}</h2>
+    <h2>{{ user }}</h2>
+    <h2>{{ user.text }}</h2>
   </div>
 </template>
