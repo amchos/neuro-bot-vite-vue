9ef72c0c4eb5b90c714fdc403d015c5eebca91a8
amchos <124582071+amchos@users.noreply.github.com>
2025-11-23 23:19:48 +0400

    hashModeFix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/neuro-bot-vite-vue/',
+  base: process.env.NODE_ENV === 'production' ? '/neuro-bot-vite-vue/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
